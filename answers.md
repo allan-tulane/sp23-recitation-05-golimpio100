10121398fc8259eb8d3bed8b90f282145a284710
golimpio <123198782+golimpio100@users.noreply.github.com>
2023-03-22 03:31:34 +0000

    all

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
-# CMPS 2200 Reciation 5
+# CMPS 2200 Recitation 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Griffin Olimpio and Devin Gutierrez
 
 
 Place all written answers from `recitation-06.md` here for easier grading.
@@ -13,8 +13,26 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 
 - **1b.**
+We compared both versions of qsort (with a fixed and random pivot) with both ssort and timsort. As expected, ssort was the slowest of them all and as n increases, it became exponentially slower than the qsorts. The runtimes of each are similar to their bounds which further supports our data. Random pivot quicksort was also faster than fixed pivot quicksort, showing that its asymptotic runtime is faster.
+
+Sorted
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |   tim-sort |
+|--------|---------------------|----------------------|------------|
+|    100 |               0.002 |                0.069 |      0.049 |
+|    200 |               0.001 |                0.004 |      0.004 |
+|    500 |               0.001 |                0.006 |      0.014 |
+|   1000 |               0.001 |                0.006 |      0.234 |
+|   2000 |               0.001 |                0.007 |      0.284 |
+|   5000 |               0.002 |                4.720 |      0.122 |
+|  10000 |               0.002 |                0.008 |      0.554 |
+|  20000 |               0.003 |                0.008 |      0.295 |
+|  50000 |               0.005 |                0.014 |      3.118 |
+| 100000 |               0.004 |                0.015 |      2.286 |
+ 
 
 
 
 
 - **1c.**
+
+We compared the tim-sort within Python to our fastest other sorting method from 1b (which was qsort when it had a random pivot opposed to a fixed one). This is because it could adapt easier to get the fastest runtime while sorting without being restrained to a fixed pivot. However, after comparing it with Timsort, it turns out that timsort is even faster because it can recognize ordered pairs in the list that it does not have to reorder (unlike qsort or ssort that reorder each), which reduces the amount of work and decreases the runtime. 
--- main.py
+++ main.py
@@ -3,23 +3,36 @@ import tabulate
 
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+    if len(a) <= 1:
+        return a
+    else:
+        pivot = pivot_fn(a)
+        left = [x for x in a if x < pivot]
+        mid = [x for x in a if x == pivot]
+        right = [x for x in a if x > pivot]
+        return qsort(left, pivot_fn) + mid + qsort(right, pivot_fn)
+
+
+def ssort(a):
+    L = list(a)
+    for i in range(len(L)):
+        #print(L)
+        m = L.index(min(L[i:]))
+        L[i], L[m] = L[m], L[i]
+    return L                                              
+
+
 def time_search(sort_fn, mylist):
     """
     Return the number of milliseconds to run this
     sort function on this list.
-
     Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
+    Note 2: time.time() returns the current time in seconds.
     You'll have to multiple by 1000 to get milliseconds.
-
     Params:
       sort_fn.....the search function
       mylist......the list to search
-      key.........the search key 
-
+      key.........the search key
     Returns:
       the number of milliseconds it takes to run this
       search function on this input.
@@ -27,12 +40,11 @@ def time_search(sort_fn, mylist):
     start = time.time()
     sort_fn(mylist)
     return (time.time() - start) * 1000
-    ###
+
 
 def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
     """
     Compare the running time of different sorting algorithms.
-
     Returns:
       A list of tuples of the form
       (n, linear_search_time, binary_search_time)
@@ -40,9 +52,10 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    qsort_fixed_pivot = lambda a: a[0]
+    qsort_random_pivot = lambda a: random.choice(a)
+    tim_sort = lambda a: sorted(a)
+    select_sort = lambda a: ssort(a)
     result = []
     for size in sizes:
         # create list in ascending order
@@ -53,19 +66,22 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
             len(mylist),
             time_search(qsort_fixed_pivot, mylist),
             time_search(qsort_random_pivot, mylist),
+            time_search(tim_sort, mylist), time_search(select_sort, mylist)
         ])
     return result
-    ###
+
 
 def print_results(results):
     """ change as needed for comparisons """
     print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
+                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'tim-sort','ssort'],
                             floatfmt=".3f",
                             tablefmt="github"))
 
+
 def test_print():
     print_results(compare_sort())
+
 
 random.seed()
 test_print()
